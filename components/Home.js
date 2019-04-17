63218619c0c9938b4c738ef07c1825dd49d93e6a
mmerezhko <mmerezhko@rendmate.com>
2019-04-17 15:17:42 +0300

    Fetch data from firebase

--- components/Home.js
+++ components/Home.js
@@ -1,33 +1,63 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
-import { LinearGradient } from 'expo';
+import React, { Component } from "react";
+import {
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+  View
+} from "react-native";
+import { LinearGradient } from "expo";
 
-import Logo from './Logo';
+import { firebase } from "../config/env";
+
+import Logo from "./Logo";
 
 class Home extends Component {
   static navigationOptions = {
-    title: 'Home'
+    title: "Home"
   };
+
+  state = {
+    events: []
+  };
+
+  componentDidMount() {
+    firebase.ref("/events").on("value", snapshot => {
+      let data = snapshot.val();
+      let items = Object.values(data);
+
+      this.setState({ events: items });
+    });
+  }
 
   render() {
     const { navigate } = this.props.navigation;
 
     return (
-      <View style={styles.container}>
+      <ScrollView style={styles.container}>
         <Logo />
-        <LinearGradient
-          colors={['#60E6FF', '#58A5FF']}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-          style={styles.item}
-        />
+        {this.state.events.map((item, index) => (
+          <LinearGradient
+            key={index}
+            colors={["#60E6FF", "#58A5FF"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 1 }}
+            style={styles.item}
+          >
+            <View style={styles.card}>
+              <Text style={styles.head}>{item.title}</Text>
+              <Text style={styles.short}>{item.location}</Text>
+              <Text>{item.date}</Text>
+            </View>
+          </LinearGradient>
+        ))}
         <TouchableOpacity
           style={styles.btn}
-          onPress={() => navigate('Profile')}
+          onPress={() => navigate("Profile")}
         >
           <Text style={styles.text}>Profile</Text>
         </TouchableOpacity>
-      </View>
+      </ScrollView>
     );
   }
 }
@@ -35,7 +65,7 @@ class Home extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     padding: 16
   },
   item: {
@@ -45,17 +75,30 @@ const styles = StyleSheet.create({
     marginVertical: 20
   },
   btn: {
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     borderWidth: 2,
-    borderColor: '#000',
+    borderColor: "#000",
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 50,
     width: 140
   },
+  card: {
+    padding: 20
+  },
+  head: {
+    color: "#fff",
+    fontSize: 26,
+    fontWeight: "900"
+  },
+  short: {
+    color: "#fff",
+    fontSize: 14,
+    marginVertical: 10
+  },
   text: {
     fontSize: 15,
-    textAlign: 'center'
+    textAlign: "center"
   }
 });
 
